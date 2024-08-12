06d1fbd2e065c37f76445667e0b240a85d9037a6
joaocarloslima <joaocarloslima@me.com>
2024-08-12 20:59:21 -0300

    post users

--- src/main/java/br/com/fiap/apisphere/user/UserController.java
+++ src/main/java/br/com/fiap/apisphere/user/UserController.java
@@ -1,9 +1,10 @@
 package br.com.fiap.apisphere.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -17,6 +18,20 @@ public class UserController {
     @GetMapping
     public List<User> findAll(){
         return service.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user, UriComponentsBuilder uriBuilder){
+        service.create(user);
+
+        var uri = uriBuilder
+                    .path("/users/{id}")
+                    .buildAndExpand(user.getId())
+                    .toUri();
+
+        return ResponseEntity
+                    .created(uri)
+                    .body(user);
     }
 
 }
